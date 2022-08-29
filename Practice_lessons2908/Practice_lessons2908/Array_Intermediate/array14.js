324c892d30d559c1b0520de97cfae2b705d219b0
hasanmd91 <hasanmd1991@hotmail.com>
2022-08-29 21:01:01 +0300

    task 14 done

--- Practice_lessons2908/Practice_lessons2908/Array_Intermediate/array14.js
+++ Practice_lessons2908/Practice_lessons2908/Array_Intermediate/array14.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
 Complete the function getYear such that it returns the searchYear (passed as 2nd parameter) when it's found in the array.
@@ -9,7 +9,8 @@ Otherwise, it should return undefined.
  * @param {number} searchYear
  */
 function getYear(years, searchYear) {
-
+  const filteredArray = years.find((elemet) => elemet === searchYear);
+  return filteredArray;
 }
 
 // Sample usage - do not modify
